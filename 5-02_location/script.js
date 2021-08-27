934208075db22de54d7c00cc4cc19e72dc9cb13b
yamanoha-hab <r12m097@yahoo.co.jp>
2021-08-27 18:51:36 +0900

    css読み込み、 復習コメントアウト タイトル記入

--- 5-02_location/script.js
+++ 5-02_location/script.js
@@ -1,7 +1,9 @@
 'use strict';
 
+// HTML要素のlang属性の値を代入。
 const lang = document.querySelector('html').lang;
 
+// オプションのテキストをページに合わせる。
 if(lang === 'ja') {
   document.querySelector('option[value="index.html"]').selected = true;
 } else if(lang === 'en') {
@@ -10,6 +12,9 @@ if(lang === 'ja') {
   document.querySelector('option[value="index-zn.html"]').selected = true;
 }
 
+// フォームの内容が変わった時イベント発生
 document.getElementById('form').select.onchange = function() {
+  // Location.hrefは表示しているページのURL = 新しいURL
+  // プルダウンメニューの場合は、選択されているoptionのvalue属性を調べるために、その親要素であるselectのvalueプロパティを読み取る。
   location.href = document.getElementById('form').select.value;
 }
